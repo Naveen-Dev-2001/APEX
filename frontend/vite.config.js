30952d566ffa34ea08c925e46e789bad5aa8af90
Naveen <naveenkumarm572@gmail.com>
2026-03-20 21:19:33 +0530

    config issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,30 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-  viteStaticCopy({
-    targets: [
-      {
-        src: 'node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
-        dest: ''
-      }
-    ]
-  })
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
   server: {
     port: 4000,
     historyApiFallback: true,
   },
-  optimizeDeps: {
-    include: ['react-pdf', 'pdfjs-dist']
-  },
-  resolve: {
-    dedupe: ['react', 'react-dom'],
-    alias: {
-      react: path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-    }
-  }
 })
